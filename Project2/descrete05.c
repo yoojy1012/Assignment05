24426aaef38203fffbeed2452879cd62579e3809
Ju Yeong Yoo <yoojy1012@gmail.com>
2019-11-07 01:21:22 +0900

    01

---
+++ Project2/descrete05.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> 
+
+int sum(int* pNumbers, int nIndex)
+{
+	if (nIndex <= 0)
+	{
+		return nIndex;
+	}
+	else
+	{
+		return pNumbers + sum(pNumbers, nIndex);
+	}
+}
+
+int main(int argc, char* argv)
+{
+
+}
