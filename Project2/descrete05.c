4c8e2730d46baf667c4e7668b393591f121a554b
Ju Yeong Yoo <yoojy1012@gmail.com>
2019-11-07 22:56:24 +0900

    05

--- Project2/descrete05.c
+++ Project2/descrete05.c
@@ -7,10 +7,12 @@ int sum(int* pNumbers, int nIndex)
 	if (nIndex <= 0)
 	{
 		return nIndex;
+		printf("%d\n", nIndex);
 	}
 	else
 	{
 		return pNumbers + sum(pNumbers, nIndex);
+		printf("%d\n", *pNumbers);
 		nIndex--;
 	}
 }
@@ -20,8 +22,31 @@ int main(int argc, char* argv[])
 	FILE* fp1, * fp2;
 	char str[100];
 
-	fp1 = fopen("argv[1]", "r");
-	fp2 = fopen("argv[2]", "w+");
+	if (fp1 = fopen(argv[1], "rt") == NULL) {
+		printf("fail to open file.");
+		return 0;
+	}
+	if (fp2 = fopen(argv[2], "wt") == NULL) {
+		printf("fail to create file for write.");
+		return 0;
+	}
+
+	int nNumber = 30;
+	int pNumbers[30];
+
+	for (int i = 0; i < nNumber; i++)
+	{
+		pNumbers[i] = i;
+	}
 
 
+	int nSum = sum(pNumbers, nNumber);
+
+	printf("%d\n", nSum);
+
+	free(pNumbers);
+
+	fclose(fp1);
+	fclose(fp2);
+	return 0;
 }
